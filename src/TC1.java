1b1e969f6ced961ca3d42fee0da89363d4f5eedd
ngaanh124 <ngaanh124@gmail.com>
2025-06-25 00:43:43 +0700

    Nga code 2

--- src/TC1.java
+++ src/TC1.java
@@ -2,5 +2,6 @@ public class TC1 {
     public static void main(String[] args) {
         System.out.println("Log in Test");
         System.out.println("Code 1");
+        System.out.println("Code 2");
     }
 }
